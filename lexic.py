a3b32d48fd7f209887f0b2bea91adfdb7687b704
Erik Martinez <61127579+ErikMtz99@users.noreply.github.com>
2021-03-30 22:14:34 -0600

    Update lexic.py

--- lexic.py
+++ lexic.py
@@ -2,45 +2,107 @@ import ply.lex as lex
 import ply.yacc as yacc
 import sys
 
+reserved = {
+    'start': 'START',
+    'end' : 'END',
+    'dim' : 'DIM',
+    'is' : 'IS',
+    'if' : 'IF',
+    'then' : 'THEN',
+    'else' : 'ELSE',
+    'when' : 'WHEN',
+    'do' : 'DO',
+    'for' : 'FOR',
+    'to' : 'TO',
+    'gosub' : 'GOSUB',
+    'go' : 'GO',
+    'input' : 'INPUT',
+    'output' : 'OUTPUT',
+    'word' : 'WORD',
+    'float' : 'FLOAT',
+    'sub procedure' : 'SUB PROCEDURE',
+    'return' : 'RETURN',
+    'and' : 'AND',
+    'or' : 'OR',
+    'not' : 'NOT',
+        
+ }
+
 tokens = [      #Todos los tokens que se vayan a usar
-    'HOLA',
+    'ID',
+    'CTE',
+    'WORD',
+    'FLOAT',
+    '#',
+
+    
+    'SUMA',
+    'RESTA',
+    'MULT',
+    'DIV',
+    'IZQPAR',
+    'DERPAR',
+    'IZQCORCH',
+    'DERCHORCH',
+    'LINK',
     'COMA',
-    'QUE',
-    'TAL',
-    'NUMERO'
-]
+    'COMMENT'
+        
+] + list(reserved.values())
 
-t_ignore = r' '  #Ignoramos los espacios
+literals = "+-/*()[], ;:^<>.|" 
 
-def t_HOLA(t):
-    r'hola'
-    t.type = 'HOLA'
-   # print("HOLA")
+#Ignoramos los espacios
+t_ignore = r' '  
+ 
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_\d\[\]]*'
+    t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
+
+def t_CTE(t):
+        r'\d+'
+        t.value = int(t.value)    
+        return t 
+    
+def t_WORD(t):
+    r'\"[a-z A-Z_\d]+\"'
+    t.value = str(t.value)
+    return t    
+
+def t_FLOAT(t):
+        r'\d+\.\d+'
+        t.value = float(t.value)    
+        return t 
+    
+ #Expresiones Regulares para tokens simples
+t_SUMA    = r'\+'
+t_RESTA   = r'-'
+t_MULT   = r'\*'
+t_DIV  = r'/'
+t_IZQPAR  = r'\('
+t_DERPAR  = r'\)'
+t_IZQCORCH  = r'\['
+t_DERCORCH  = r'\]'
+t_LINK = r'\<\='   
 
 def t_COMA(t):
     r'\,'
     t.type = 'COMA'
    # print(",")
     return t
+ 
+#Comentarios (//) 
+def t_COMMENT(t): 
+    r'\//.*'
+    pass
+    
+# Define a rule so we can track line numbers
+def t_newline(t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)   
 
-def t_QUE(t):
-    r'que'
-    t.type = 'QUE'
-    #print("QUE")
-    return t
-
-def t_TAL(t):
-    r'tal'
-    t.type = 'TAL'
-   # print("TAL")
-    return t
-
-def t_NUMBER(t):
-        r'\d+'
-        t.value = int(t.value)    
-        return t
-
+# Error handling rule
 def t_error(t):
     print("Illegal characters!")
     t.lexer.skip(1)
