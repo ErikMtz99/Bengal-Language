c2afbeb4cbe78e3989ade8ebc2c084e99b577bb3
Erik Martinez <61127579+ErikMtz99@users.noreply.github.com>
2021-03-30 23:27:59 -0600

    Update lexic.py
    Ya funcionan las palabras en el orden que se quiere, pero no hay funcionalidad todavia.

--- lexic.py
+++ lexic.py
@@ -23,6 +23,8 @@ reserved = {
     'and' : 'AND',
     'or' : 'OR',
     'not' : 'NOT',
+    'word' : 'WORD',
+    'float' : 'FLOAT',
     '#' : 'WHERE'
         
  }
@@ -30,8 +32,8 @@ reserved = {
 tokens = [      #Todos los tokens que se vayan a usar
     'ID',
     'CTE',
-    'WORD',
-    'FLOAT',
+#    'WORD',
+#    'FLOAT',
 
     
     'SUMA',
@@ -44,11 +46,14 @@ tokens = [      #Todos los tokens que se vayan a usar
     'DERCORCH',
     'LINK',
     'COMA',
-    'COMMENT'
+    'COMMENT',
+    
+    'EQUAL',
+    'NOTEQUAL'
         
 ] + list(reserved.values())
 
-literals = "+-/*()[], ;:^<>.|" 
+literals = "+-/*()[],!;:^<>.|" 
 
 #Ignoramos los espacios
 t_ignore = r' '  
@@ -63,15 +68,15 @@ def t_CTE(t):
         t.value = int(t.value)    
         return t 
     
-def t_WORD(t):
-    r'\"[a-z A-Z_\d]+\"'
-    t.value = str(t.value)
-    return t    
+#def t_WORD(t):
+#    r'\"[a-z A-Z_\d]+\"'
+#    t.value = str(t.value)
+#    return t    
 
-def t_FLOAT(t):
-        r'\d+\.\d+'
-        t.value = float(t.value)    
-        return t 
+#def t_FLOAT(t):
+#        r'\d+\.\d+'
+#        t.value = float(t.value)    
+#        return t 
     
  #Expresiones Regulares para tokens simples
 t_SUMA    = r'\+'
@@ -83,16 +88,14 @@ t_DERPAR  = r'\)'
 t_IZQCORCH  = r'\['
 t_DERCORCH  = r'\]'
 t_LINK = r'\<\='   
+t_COMA = r'\,'
 
-def t_COMA(t):
-    r'\,'
-    t.type = 'COMA'
-   # print(",")
-    return t
+t_EQUAL = r'\=\='
+t_NOTEQUAL = r'\!\='
  
 #Comentarios (//) 
 def t_COMMENT(t): 
-    r'\//.*'
+    r'\/\/.*'
     pass
     
 # Define a rule so we can track line numbers
@@ -110,23 +113,107 @@ lexer = lex.lex()
 
 ##################################################
 
-def p_S(p):
+def p_programa(p):
 	'''
-	S : X QUE TAL 
+	programa : START variables procedures main END 
 	'''
 	print("\tCORRECTO")
 
-def p_X(p):
+def p_variables(p):
 	'''
-	X : HOLA Y
-	'''
-
-def p_Y(p):
-	'''
-	Y : COMA HOLA Y
-	  | 
-	'''
+	variables : DIM A
+              | 
+              
+    A : A B C IS tipo ';'
+      | B C IS tipo ';'
       
+    B : ID COMA B
+      | ID
+    
+    C : IZQCORCH D DERCORCH
+      |
+    
+    D : CTE COMA D
+      | CTE
+      
+    tipo : WORD
+         | FLOAT
+	'''
+    
+def p_procedures(p):
+    '''
+	procedures : SUBPROCEDURE ID main RETURN ';' procedures
+               |
+	'''
+def p_main(p):
+    '''
+	main : E
+    
+    E : E F S ';'
+      | F S ';'
+    
+    F : WHERE ID ':'
+      |
+	'''  
+
+def p_S(p):
+    '''
+	S : var LINK EA COMMENT
+      | INPUT var
+      | OUTPUT WORD
+      | IF EL THEN G ELSE G END
+      | WHEN EL DO G END
+      | DO G WHEN EL END
+      | FOR ID LINK EA TO EA DO G END
+      | GO WHERE ID
+      | GOSUB ID
+      | EA
+      |
+    
+    G : G S
+      | S
+     
+    var : ID
+        | ID IZQCORCH J DERCORCH 
+
+    J : EA COMA J
+    J : EA
+	'''  
+    
+def p_EA(p):
+    '''
+	EA : EA SUMA TA
+       | EA RESTA TA
+       | TA
+       
+    TA : TA MULT FA
+       | TA DIV FA
+       | FA
+    
+    FA : CTE
+       | ID
+       | IZQPAR EA DERPAR
+    
+	'''  
+def p_EL(p):
+    '''
+	EL : EL OR TL
+       | EL AND TL
+       | TL
+       
+    TL : TL NOT FL
+       | FL
+    
+    FL : H '>' H
+       | H '<' H
+       | H EQUAL H
+       | H NOTEQUAL H
+       | IZQPAR EL DERPAR
+    
+    H : ID
+      | CTE
+	'''  
+    
 def p_error(p):
 	print("\tINCORRECTO")
 
